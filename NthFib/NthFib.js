d3cfed25713305ad49538b53405f9ded6d26a23e
Lucas Armstrong <17970245+LucasArmstrong@users.noreply.github.com>
2021-12-08 13:56:58 -0500

    add isFib, outputs sequence position

--- NthFib/NthFib.js
+++ NthFib/NthFib.js
@@ -12,4 +12,18 @@ function NthFib(n) {
     return fib;
 }
 
-module.exports = NthFib;
+function IsFib(number) {
+    let nth = 0;
+    let fib = NthFib(nth);
+    while (fib <= number) {
+        if (fib === number) {
+            return nth;
+        }
+        nth = nth + 1;
+        fib = NthFib(nth);
+    }
+    return false;
+}
+
+module.exports.NthFib = NthFib;
+module.exports.IsFib = IsFib;
--- NthFib/NthFib.test.js
+++ NthFib/NthFib.test.js
@@ -1,4 +1,4 @@
-const NthFib = require('./NthFib');
+const {NthFib, IsFib} = require('./NthFib');
 
 describe ('Finds correct nth fibonacci number', () => {
     test ('take number and return correct nth in fib sequence', () => {
@@ -10,5 +10,14 @@ describe ('Finds correct nth fibonacci number', () => {
         expect(NthFib(100)).toBe(354224848179262000000);
         expect(NthFib(200)).toBe(2.8057117299251016e+41);
         expect(NthFib(1000)).toBe(4.346655768693743e+208);
+    });
+})
+
+describe ('Determines if number is part of fib sequence', () => {
+    test ('Test if number is included in the fib sequence', () => {
+        expect(IsFib(1)).toBe(1);
+        expect(IsFib(11)).toBeFalsy();
+        expect(IsFib(354224848179262000000)).toBe(100);
+        expect(IsFib(43466557686937430000000000000000000000000000000000000000000)).toBeFalsy();
     });
 })
