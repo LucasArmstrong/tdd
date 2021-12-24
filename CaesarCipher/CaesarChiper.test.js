d2baa26cdb1ca3472d6ce60d0d681753e9da148f
Lucas Armstrong <17970245+LucasArmstrong@users.noreply.github.com>
2021-12-24 15:24:32 -0500

    cipher updates

--- CaesarCipher/CaesarChiper.test.js
+++ CaesarCipher/CaesarChiper.test.js
@@ -1,4 +1,4 @@
-const { CipherEncryptor } = require('./CaesarCipher');
+const { CipherEncryptor, CipherDecryptor } = require('./CaesarCipher');
 
 describe ('Caesar Cipher encoding and decoding of messages', () => {
     test ('encode messages', () => {
@@ -6,8 +6,20 @@ describe ('Caesar Cipher encoding and decoding of messages', () => {
         expect(CipherEncryptor('abc', 0)).toBe('abc');
         expect(CipherEncryptor('abc', 3)).toBe('def');
         expect(CipherEncryptor('xyz', 5)).toBe('cde');
+        expect(CipherEncryptor('def', 25)).toBe('cde');
 
         expect(CipherEncryptor('abc', 0, 'abc456')).toBe('abc');
         expect(CipherEncryptor('abc', 2, 'abc456')).toBe('c45');
+    });
+
+    test ('decode messages', () => {
+        expect(CipherDecryptor('zab', 2)).toBe('xyz');
+        expect(CipherDecryptor('abc', 0)).toBe('abc');
+        expect(CipherDecryptor('def', 3)).toBe('abc');
+        expect(CipherDecryptor('cde', 5)).toBe('xyz');
+        expect(CipherDecryptor('cde', 25)).toBe('def');
+
+        expect(CipherDecryptor('abc', 0, 'abc456')).toBe('abc');
+        expect(CipherDecryptor('c45', 2, 'abc456')).toBe('abc');
     });
 });
--- CaesarCipher/CaesarCipher.js
+++ CaesarCipher/CaesarCipher.js
@@ -11,7 +11,19 @@ function CipherEncryptor(string, key, chars = 'abcdefghijklmnopqrstuvwxyz') {
 }
 
 function CipherDecryptor(string, key, chars = 'abcdefghijklmnopqrstuvwxyz') {
-    
+    let charset = chars.split('');
+    let stringArray = string.split('');
+    for (let i = 0; i < stringArray.length; i++) {
+        let charIndex = charset.indexOf(stringArray[i]);
+        let newIndex = charIndex - key;
+        if (newIndex >= 0) {
+            stringArray[i] = charset[newIndex % charset.length];
+        } else {
+            stringArray[i] = charset[newIndex + charset.length];
+        }
+    }
+    return stringArray.join('');
 }
 
 module.exports.CipherEncryptor = CipherEncryptor;
+module.exports.CipherDecryptor = CipherDecryptor;
